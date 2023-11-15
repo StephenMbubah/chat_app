15c9b427dbb7411655d56c75149e344cb92ea313
StephenMbubah <mbubahs@gmail.com>
2023-11-15 16:02:29 +0100

    first start

---
+++ lib/shared/constants.dart
@@ -0,0 +1,7 @@
+class Constants{
+  static String appId ='1:9471374114:web:71a39f85c4d4d9b2bc6b7b';
+  static String apiKey ='AIzaSyDl9jGGaIfFIyW4KkPVqhC4Uz5PtTG5ZbI';
+  static String messagingSenderId ='9471374114';
+  static String projectId ='flutter-chat-app-317f7';
+
+}
