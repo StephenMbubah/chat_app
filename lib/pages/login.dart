421c92a02e99e6595e31063efbacc40ec38ed752
StephenMbubah <mbubahs@gmail.com>
2023-11-20 14:30:10 +0100

    first start

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,11 +5,24 @@ class LoginPage extends StatefulWidget {
 
   @override
   State<LoginPage> createState() => _LoginPageState();
+
 }
 
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body:  SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: 10, bottom: 10, top: 10, right: 10
+              msms cmckc skcmskmcm scnsjncs csm xjnsc cs mxjsnx njsxmsjxnxhnchnsbnmsnxjhsnbbcshcncshx jnnxjsnxjxsjsnsjhbhcnsc
+              snsjncjnsncnjsndncnjsnnshiunzou wo sasageyo
+              tum tum tum tum shin wa sui karo no wo etin no caro , fo sheki karo no mo hei tin no tal ,la kei you , tim mina as an ko kiwan shogeki no go wo sita e tutyo na ra came ka eeiii kjdj
+              ahnd n djjen bd akkfmjiendf bakidfnfnfjewbnd  b
+          ),
+        ),
+      ),
+    );
   }
 }
